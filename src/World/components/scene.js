81b6e1a0920bc32ae79eb2019134d1050e18a84b
ChrisBlanks <cablanks@hotmail.com>
2023-06-25 13:03:11 -0400

    fix scene texture import

--- src/World/components/scene.js
+++ src/World/components/scene.js
@@ -1,5 +1,7 @@
 import { Color, Scene, TextureLoader} from "three";
 
+const textureBaseUri = window.location + 'assets/textures/';
+
 function createScene(){
     const scene = new Scene;
     //const defaultBGColor = 'black';
@@ -7,7 +9,7 @@ function createScene(){
 
     
     const loader = new TextureLoader();
-    const sceneTexture = loader.load('/assets/textures/2k_stars.jpg', (texture) => {
+    const sceneTexture = loader.load(textureBaseUri+'2k_stars.jpg', (texture) => {
         scene.background = texture;
     });
     return scene;
